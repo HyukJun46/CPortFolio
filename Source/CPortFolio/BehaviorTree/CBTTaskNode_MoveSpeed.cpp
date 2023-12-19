e5531bcaff3f98f90f970cbf93d011514838e1a7
HyukJun <126559827+HyukJun46@users.noreply.github.com>
2023-12-19 14:35:19 +0900

    .

--- Source/CPortFolio/BehaviorTree/CBTTaskNode_MoveSpeed.cpp
+++ Source/CPortFolio/BehaviorTree/CBTTaskNode_MoveSpeed.cpp
@@ -9,6 +9,17 @@ UCBTTaskNode_MoveSpeed::UCBTTaskNode_MoveSpeed()
 
 EBTNodeResult::Type UCBTTaskNode_MoveSpeed::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-    Super::ExecuteTask(OwnerComp, NodeMemory);
 
+    AAIController* controller = OwnerComp.GetAIOwner();
+    if (controller == nullptr) return EBTNodeResult::Failed;
+
+    ACEnemy_Girl* enemy = Cast<ACEnemy_Girl>(controller->GetPawn());
+    if (enemy == nullptr) return EBTNodeResult::Failed;
+
+    UCStatusComponent* statusComp = Cast<UCStatusComponent>(enemy->GetComponentByClass(UCStatusComponent::StaticClass()));
+    if (statusComp == nullptr) return EBTNodeResult::Failed;
+
+    statusComp->ChangeMoveSpeed(Type);
+
+    return EBTNodeResult::Succeeded;
 }
