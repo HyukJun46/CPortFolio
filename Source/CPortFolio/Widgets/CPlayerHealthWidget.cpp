c2e59d0fd1be0f93ab9578fdcfaf1bc5adef0e76
HyukJun <126559827+HyukJun46@users.noreply.github.com>
2024-01-05 16:15:50 +0900

    PlayerHealthWidget

---
+++ Source/CPortFolio/Widgets/CPlayerHealthWidget.cpp
@@ -0,0 +1,2 @@
+#include "CPlayerHealthWidget.h"
+
---
+++ Source/CPortFolio/Widgets/CPlayerHealthWidget.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CPlayerHealthWidget.generated.h"
+
+UCLASS()
+class CPORTFOLIO_API UCPlayerHealthWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
